2c19483a67598c72f42dca2a3cd109cbc7f0d3a5
rjonsson <rjonssonovik@gmail.com>
2021-02-10 16:42:37 +0100

    Fixed Not found-function

--- modules/wikisearch.py
+++ modules/wikisearch.py
@@ -5,7 +5,8 @@ def get_link(query):
   response = requests.get('https://valheim.fandom.com/wiki/Special:Search?query=' + query)
   soup = BeautifulSoup(response.content, 'html.parser')
 
-  a = soup.find('li', {"class": "unified-search__result"}).find('a')
-  if a is None:
+  li = soup.find('li', {"class": "unified-search__result"})
+  if li is None:
     return (None, None)
+  a = li.find('a')
   return (a['data-title'], a['href'])
